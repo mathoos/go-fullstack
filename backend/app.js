18dd0bb56cc71a197a57fdd84c218388ac6f8a3c
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-02-01 11:38:18 +0100

    POST Things bdd

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-
 const mongoose = require('mongoose');
 
-const app = express();
+const Thing = require('./models/Thing');
+
 
 mongoose.connect('mongodb+srv://mathilde:bonjour@cluster1.6jafvbs.mongodb.net/?retryWrites=true&w=majority')
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -11,8 +11,11 @@ mongoose.connect('mongodb+srv://mathilde:bonjour@cluster1.6jafvbs.mongodb.net/?r
   // faire fichier .env avec variables
   // importer dotenv
 
+
+const app = express();
+
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*'); // à rajouter dans le .env
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
@@ -23,11 +26,14 @@ app.use(express.json());
 
 
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message : "Objet créé !"
-    })
-})
+    delete req.body._id;
+    const thing = new Thing({
+      ...req.body
+    });
+    thing.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
 
 
 app.get('/api/stuff', (req, res, next) => {
