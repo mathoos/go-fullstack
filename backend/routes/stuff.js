39abe7bd79f87c75e415011340642bc0809e7fad
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-02-02 10:13:05 +0100

    add controllers

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,39 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const Thing = require('../models/Thing');
 
-router.post('/', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    });
-    thing.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-});
+const stuffCtrl = require('../controllers/stuff');
 
-router.put('/:id', (req, res, next) => {
-    Thing.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
-        .then(() => res.status(200).json({message: 'Objet modifié !'}))
-        .catch(error => res.status(404).json({ error }));
-})
+router.post('/', stuffCtrl.createThing);
 
-router.delete('/:id', (req, res, next) => {
-    Thing.deleteOne({_id: req.params.id})
-        .then(() => res.status(200).json({message: 'Objet supprimé !'}))
-        .catch(error => res.status(404).json({ error }));
-})
+router.put('/:id', stuffCtrl.modifyThing)
 
-router.get('/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(404).json({ error }));
-});
+router.delete('/:id', stuffCtrl.deleteThing)
 
-router.get('/', (req, res, next) => {
-    Thing.find()
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(400).json({ error }));
-});
+router.get('/:id', stuffCtrl.getOneThing);
+
+router.get('/', stuffCtrl.getAllThings);
 
 module.exports = router;
