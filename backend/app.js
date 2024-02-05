89fa4149172fccc409bb2ffbd3d89684cc522ce7
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-02-05 15:57:46 +0100

    update routes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
+const path = require('path');
 
 
 mongoose.connect('mongodb+srv://mathilde:bonjour@cluster1.6jafvbs.mongodb.net/?retryWrites=true&w=majority')
@@ -26,6 +27,7 @@ app.use(express.json());
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
 
