8b6bb0b0a873278a56fc867903539aa0d638192a
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-02-07 09:26:24 +0100

    .env

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
 const path = require('path');
+const databaseUrl = process.env.DATABASE_URL;
 
 
-mongoose.connect('mongodb+srv://mathilde:bonjour@cluster1.6jafvbs.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(databaseUrl)
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-  // faire fichier .env avec variables
-  // importer dotenv
 
 
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // à rajouter dans le .env
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Origin', ACCESS_CONTROL_ALLOW_ORIGIN);
+    res.setHeader('Access-Control-Allow-Headers', ACCESS_CONTROL_ALLOW_HEADERS);
+    res.setHeader('Access-Control-Allow-Methods', ACCESS_CONTROL_ALLOW_METHODS);
     next();
 });
 
